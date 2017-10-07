417f5b7aeb3bbcd4e4d950a06dcd7c0736444dc2
Tuandl <anhtuan4597@gmail.com>
2017-10-07 16:29:53 +0700

    devide js file

--- frontend/assets/js/admin-home-page.js
+++ frontend/assets/js/admin-home-page.js
@@ -1,31 +1,3 @@
 $(document).ready(function() {
     autoGenIndexForTable();
 });
-
-/**
- * This function automatic find any table contain 
- * "#" in the first column of header
- * Example: 
- * <table>
- * <thead>
- * <tr>
- * <th>#</th>
- * </tr>
- * ...
- * </thead>
- * ...
- * </table>
- */
-function autoGenIndexForTable() {
-    $("table").each(function(index, element) {
-        // element == this
-        if ($(element).find("th:first-child").text().trim() == "#") {
-            var count = 0;
-            $(this).find("tbody").children("tr").each(function(index, element) {
-                // element == this
-                count++;
-                $(this).children("td:first").text(count);
-            });
-        }
-    });
-}
